4855494513b843142493c0d4aced972ec19a8965
John Rayes <live627@gmail.com>
2015-04-28 00:18:06 -0700

    Less code for the same result

--- src/dateinput.js
+++ src/dateinput.js
@@ -192,9 +192,7 @@
 
 		isSameDay = function (d1, d2)
 		{
-			return d1.getFullYear() === d2.getFullYear() &&
-				d1.getMonth() == d2.getMonth() &&
-				d1.getDate() == d2.getDate();
+			return d1.toDateString() == d2.toDateString();
 		},
 
 		parseDate = function (val)
