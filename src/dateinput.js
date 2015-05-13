a24d6b4e05a6d36e015d32b530741d89b855d43d
John Rayes <live627@gmail.com>
2015-05-12 22:50:21 -0700

    https://raw.githubusercontent.com/Raynos/xtend/master/mutable.js

--- src/dateinput.js
+++ src/dateinput.js
@@ -322,4 +322,18 @@
 		});
 	};
 
+	function extend (target) {
+		for (var i = 1; i < arguments.length; i++) {
+			var source = arguments[i]
+
+			for (var key in source) {
+				if (source.hasOwnProperty(key)) {
+					target[key] = source[key]
+				}
+			}
+		}
+
+		return target
+	}
+
 }) (jQuery);
